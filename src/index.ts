5c1a3560c30b16badda129d2c49924c7d09de73c
niksoper <niksoper@hotmail.com>
2018-04-08 12:28:30 +0100

    Move response handling out of scanner

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,42 @@ import { createReadStream, readdir } from 'fs'
 import path from 'path'
 import request from 'request'
 
-import { createScanner } from './scanner'
+import { createScanner, RATE_LIMIT_EXCEEDED } from './scanner'
 
 const argv = minimist(process.argv.slice(2))
 const [directory] = argv._
 
-const scanner = createScanner({
+const scannerOptions = {
   url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/2c8ede3f-87ac-4b7f-928e-506882d16fbf/image',
   key: 'dd0d0c7396414986b12d140ee1b9a095',
   retryInterval: 1500,
+}
+const scanner = createScanner(scannerOptions)
+
+readdir(directory, async (err, files) => {
+  for (let file of files) {
+    await scanFile(directory, file)
+  }
 })
 
-readdir(directory, (err, files) => {
-  files.forEach(file => {
-    const filePath = path.resolve(directory, file)
-    scanner(filePath)
-  })
-})
+async function scanFile(directory: string, name: string) {
+  const filePath = path.resolve(directory, name)
+  console.log(`Scanning ${name}...`)
+  const { response, rawBody, body } = await scanner(filePath)
+
+  console.log(`Got ${response.statusCode} from ${name}`)
+  if (response.statusCode !== 200) {
+    console.log(rawBody)
+
+    // if (response.statusCode === RATE_LIMIT_EXCEEDED) {
+    //   console.log(`Retrying in ${scannerOptions.retryInterval / 1000} seconds...`)
+    //   setTimeout(() => scan(filePath), scannerOptions.retryInterval)
+    // }
+    return
+  }
+
+  console.log(body.Predictions.map(p => {
+    const { TagId, ...rest } = p
+    return rest
+  }))
+}
--- src/scanner.ts
+++ src/scanner.ts
@@ -1,5 +1,5 @@
 import { createReadStream } from "fs"
-import request from 'request'
+import request, { RequestCallback } from 'request'
 
 export interface ScannerOptions {
   url: string
@@ -7,44 +7,46 @@ export interface ScannerOptions {
   retryInterval: number
 }
 
-interface Prediction {
+export interface Prediction {
   TagId: string
   Tag: string
   Probability: number
 }
 
-interface PredictionResponse {
+export interface PredictionResponseBody {
   Predictions: Prediction[]
 }
 
-const RATE_LIMIT_EXCEEDED = 429
+export interface PredictionResponse {
+  response: request.Response
+  rawBody: string
+  body: PredictionResponseBody
+}
+
+export const RATE_LIMIT_EXCEEDED = 429
 
 export function createScanner({ url, key, retryInterval }: ScannerOptions) {
-  return function scan(filePath: string) {
-    console.log(`PATH: ${filePath}`)
-    createReadStream(filePath).pipe(request.post({
-      url,
-      headers: {
-        'Content-Type': 'application/octet-stream',
-        'Prediction-Key': key,
-      }
-    }, (err, response, rawBody) => {
-      console.log(`${filePath} received ${response.statusCode}`)
-      if (response.statusCode !== 200) {
-        console.log(rawBody)
-  
-        if (response.statusCode === RATE_LIMIT_EXCEEDED) {
-          console.log(`Retrying in ${retryInterval / 1000} seconds...`)
-          setTimeout(() => scan(filePath), retryInterval)
+  return function scan(filePath: string): Promise<PredictionResponse> {
+    return new Promise((resolve, reject) => {
+      createReadStream(filePath).pipe(request.post({
+        url,
+        headers: {
+          'Content-Type': 'application/octet-stream',
+          'Prediction-Key': key,
         }
-        return
-      }
-  
-      const body = JSON.parse(rawBody) as PredictionResponse
-      console.log(body.Predictions.map(p => {
-        const { TagId, ...rest } = p
-        return rest
+      }, (err, response, body) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        const parsedBody = response.statusCode === 200 ? JSON.parse(body) : null
+        resolve({
+          response,
+          rawBody: body,
+          body: parsedBody
+        })
       }))
-    }))
+    })
   }
 }
